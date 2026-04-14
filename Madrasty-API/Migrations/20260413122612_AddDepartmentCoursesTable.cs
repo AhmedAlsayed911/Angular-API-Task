ce6799a83018257755d8b958ea5c49ab01a6f4d6
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-04-15 00:37:47 +0200

    feat(db): add department course migration and snapshot

---
+++ Madrasty-API/Migrations/20260413122612_AddDepartmentCoursesTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Madrasty_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartmentCoursesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DepartmentCourses",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DepartmentCourses", x => new { x.CourseId, x.DepartmentId });
+                    table.ForeignKey(
+                        name: "FK_DepartmentCourses_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DepartmentCourses_Departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DepartmentCourses_DepartmentId",
+                table: "DepartmentCourses",
+                column: "DepartmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DepartmentCourses");
+        }
+    }
+}
