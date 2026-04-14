6d5c33a5d86969dc4c2284175fed44e4292cb5a5
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-04-15 00:37:45 +0200

    feat(entities): add application user and course entities

---
+++ Madrasty-API/Entities/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Madrasty_API.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+        [MaxLength(30)]
+        public string LastName { get; set; }
+    }
+}
