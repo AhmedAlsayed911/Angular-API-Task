0633ed483f3312ed1467ec9ede881b46226e2be4
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-04-15 00:37:48 +0200

    feat(api): add launch settings and generic repository

---
+++ Madrasty-API/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using Madrasty.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Madrasty_API.Repositories
+{
+    public class GenericRepository<T>(ApplicationDbContext dbContext)
+        : IGenericRepository<T> where T : class
+    {
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await dbContext.Set<T>().FindAsync(id);
+        }
+
+    public async Task<T> AddAsync(T data)
+    {
+        await dbContext.Set<T>().AddAsync(data);
+        return data;
+    }
+
+        public async Task<T> UpdateAsync(T data)
+        {
+            dbContext.Set<T>().Update(data);
+            return data;
+        }
+
+        public void DeleteAsync(int id)
+        {
+            dbContext.Set<T>().Remove(dbContext.Set<T>().Find(id));
+        }
+
+    }
+}
