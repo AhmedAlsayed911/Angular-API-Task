21e4df51ca7a0e7f2cedc9ffb3f4dd8da0d3698e
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-04-15 00:37:42 +0200

    feat(auth): add register request model

---
+++ Madrasty-API/AuthModels/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Madrasty_API.AuthModels
+{
+    public class RegisterModel
+    {
+        [StringLength(100)]
+        public string FirstName { get; set; }
+        [StringLength(100)]
+        public string LastName { get; set; }
+        [StringLength(50)]
+        public string UserName { get; set; }
+        [StringLength(128)]
+        public string Email { get; set; }
+        [StringLength(256)]
+        public string Password { get; set; }
+    }
+}
