de7ca0a1ffc83be066f923f5aa88bde9b48ceffb
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-04-15 00:37:45 +0200

    feat(entities): add department and department course entities

---
+++ Madrasty-API/Entities/DepartmentCourse.cs
@@ -0,0 +1,12 @@
+namespace Madrasty.Entites;
+
+public partial class DepartmentCourse
+{
+    public int DepartmentId { get; set; }
+
+    public int CourseId { get; set; }
+
+    public virtual Department Department { get; set; }
+
+    public virtual Course Course { get; set; }
+}
