e2abfec0082fd2c12e9c56effbfcf9df60fa182a
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-04-15 00:37:48 +0200

    feat(api): add repository interface and unit of work

---
+++ Madrasty-API/Repositories/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Madrasty_API.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> AddAsync(T data);
+        Task<T> UpdateAsync(T data);
+        void DeleteAsync(int id);
+    }
+}
---
+++ Madrasty-API/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,59 @@
+﻿using Madrasty.Entites;
+using Madrasty_API.Repositories;
+
+namespace Madrasty_API.UnitOfWork
+{
+    public class UnitOfWork
+    {
+        private readonly ApplicationDbContext dbContext;
+        private IGenericRepository<Student> studentRepository;
+        private IGenericRepository<Department> departmentRepository;
+        private IGenericRepository<Course> courseRepository;
+        private IGenericRepository<Topic> topicRepository;
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IGenericRepository<Student> StudentRepository
+        {
+            get
+            {
+                if (studentRepository is null)
+                    studentRepository = new GenericRepository<Student>(dbContext);
+                return studentRepository;
+            }
+        }
+        public IGenericRepository<Department> DepartmentRepository
+        {
+            get
+            {
+                if (departmentRepository is null)
+                    departmentRepository = new GenericRepository<Department>(dbContext);
+                return departmentRepository;
+            }
+        }
+        public IGenericRepository<Course> CourseRepository
+        {
+            get
+            {
+                if (courseRepository is null)
+                    courseRepository = new GenericRepository<Course>(dbContext);
+                return courseRepository;
+            }
+        }
+        public IGenericRepository<Topic> TopicRepository
+        {
+            get
+            {
+                if (topicRepository is null)
+                    topicRepository = new GenericRepository<Topic>(dbContext);
+                return topicRepository;
+            }
+        }
+        public void SaveChanges()
+        {
+            dbContext.SaveChanges();
+        }
+    }
+}
