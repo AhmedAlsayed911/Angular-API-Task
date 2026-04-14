5c06056785ac016db781ca01fa7cb28eea3b6ef4
AhmedAlsayed911 <ahmad.sayed.salem11@gmail.com>
2026-04-15 00:37:46 +0200

    feat(entities): add student course and topic entities

---
+++ Madrasty-API/Entities/StudentCourse.cs
@@ -0,0 +1,14 @@
+﻿namespace Madrasty.Entites;
+
+public partial class StudentCourse
+{
+    public int StudentId { get; set; }
+
+    public int CourseId { get; set; }
+
+    public int Grade { get; set; }
+
+    public virtual Course Course { get; set; }
+
+    public virtual Student Student { get; set; }
+}
